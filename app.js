35f6936518b9e59be7b7eec668b862ebe82a2d5c
Jedamara <new.jedamara@gmail.com>
2022-08-29 10:31:31 -0500

    added path to productCart view

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const app = express();
 
 
 app.use(express.static(path.join(__dirname,'public')));
-app.use(express.static(path.join(__dirname,'views')));
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
 })
 
+app.get('/productCart', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/productCart.html'))
+})
 
 
 
