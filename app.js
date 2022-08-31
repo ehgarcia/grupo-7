6be16cf9c1a1c7a0295fd8d5b4691afe4e5b46f1
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-08-31 19:57:14 -0300

    se agrego retro.md

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.get('/productCart', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/productCart.html'))
 })
 
-
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
 
 
 app.listen('3000')
