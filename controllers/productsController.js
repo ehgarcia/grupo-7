1fa988b4779de7ad2e181d1707bca615f93911e0
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-11 17:45:53 -0300

    agregamos rutas para prducts

---
+++ controllers/productsController.js
@@ -0,0 +1,7 @@
+const controller = {
+    productCard: function(req,res){
+        res.render('products/productCart')
+    }
+}
+
+module.exports = controller
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let indexController = require ('../controllers/indexController')
 let productsController = require('../controllers/productsController')
-
-
-/* GET home page. */
-router.get('/', indexController.index);
 
 
 /* GET product. */
