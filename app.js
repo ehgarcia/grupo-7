af9222ba11701d9b3dfb854e796a0288e10f436e
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-11 16:49:12 -0300

    agregamos ejs, partials,controllers

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const express = require ('express');
-const path = require('path')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/index.html'))
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.get('/productCart', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/productCart.html'))
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.get('/register', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/register.html'))
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/login', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/login.html'))
-})
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.get('/productDetails', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/productDetails.html'))
-})
-
-app.listen('3000')
+module.exports = app;
